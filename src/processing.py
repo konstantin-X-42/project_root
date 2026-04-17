d9fd05521a00e7b9b9c71fe1f455966fa6da7bea
konstantin-X-42 <kostyani4y@gmail.com>
2026-04-17 12:14:01 +0300

    Update processing.py
    изменил функции в processing.py по замечаниям flake8 и mypy

--- src/processing.py
+++ src/processing.py
@@ -1,28 +1,34 @@
-#from src.processing import filter_by_state, sort_by_date
+from typing import Any   # подаются разные типы данных для Mypy: «выключить проверку строк где использовано Any»  
 
-def filter_by_state(date_list: list[dict] | tuple[dict] = None, state: str='EXECUTED') -> list[dict]:
-    """ функция возвращает массив с выбранным ключом state по умолчанию возвращает с 'EXECUTED' """
-    try:   # обработка исключений
+def filter_by_state(date_list: list[dict[str, Any]] | tuple[dict[str, Any]] | None = None,
+                    state: str = "EXECUTED") -> list[dict[str, Any]]:
+    """функция возвращает массив с выбранным ключом state по умолчанию возвращает с 'EXECUTED'"""
+    try:  # обработка исключений
+        if date_list is None:
+            return []  # "Error_11 - в функцию не подаются аргументы"
         state_list = []
-        for state_dict in date_list:   # итерируем словари из списка (массива)
+        for state_dict in date_list:  # итерируем словари из списка (массива)
             if state == state_dict["state"]:
-                state_list.append(state_dict)   # добавляем словарь в конец списка каждую итерацию
+                state_list.append(state_dict)  # добавляем словарь в конец списка каждую итерацию
         return state_list
-    except KeyError:   # обращение к элементу словаря (dict) по key, которого в этом словаре нет
-        return "Error_11 - в аргументе функции, key в словарях или в одном из словарей отсутствует"
-    except Exception as e:     # другая непредвиденная ошибка
-        return "Error_12 - в функцию не подаются аргументы или поданы в ином формате"
+    except KeyError:  # обращение к элементу словаря (dict) по key, которого в этом словаре нет
+        return []  # "Error_12 - в аргументе функции, key в словарях или в одном из словарей отсутствует"
+    except Exception:  # другая непредвиденная ошибка
+        return []  # "Error_13 - в функцию не подаются аргументы или поданы в ином формате"
 
 
 # --------------------------------------------------------------------
 
 
-def sort_by_date(date_list: list[dict] | tuple[dict] = None, reverse: bool=True) -> list[dict]:
-    """ функция сортирует словари по дате: на убывание - по умолчанию или на возрастание,
-    по установленному значению в аргументе - sorting """
-    try:   # обработка исключений
-        return sorted(date_list, key=lambda x: x['date'], reverse=reverse)
-    except KeyError:   # обращение к элементу словаря (dict) по key, которого в этом словаре нет
-        return "Error_13 - в аргументе функции, key в словарях или в одном из словарей отсутствует"
-    except Exception as e:     # другая непредвиденная ошибка
-        return "Error_14 - в функцию не подаются аргументы или поданы в ином формате"
+def sort_by_date(date_list: list[dict[str, Any]] | tuple[dict[str, Any]] | None = None,
+                 reverse: bool = True) -> list[dict[str, Any]]:
+    """функция сортирует словари по дате: на убывание - по умолчанию или на возрастание,
+    по установленному значению в аргументе - sorting"""
+    try:  # обработка исключений
+        if date_list is None:
+            return []     # "Error_14 - в функцию не подаются аргументы"
+        return sorted(date_list, key=lambda x: x["date"], reverse=reverse)
+    except KeyError:  # обращение к элементу словаря (dict) по key, которого в этом словаре нет
+        return []  # "Error_15 - в аргументе функции, key в словарях или в одном из словарей отсутствует"
+    except Exception:  # другая непредвиденная ошибка
+        return []  # "Error_16 - в функцию не подаются аргументы или поданы в ином формате"
